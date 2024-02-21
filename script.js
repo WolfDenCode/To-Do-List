1f7dd947f5a514b03260f57a290845b5af89ba1f
Farkas Márk <mark.farkas.207@gmail.com>
2024-02-21 12:48:18 +0100

    To-Do-List

---
+++ script.js
@@ -0,0 +1,16 @@
+let parentElement = document.querySelector(".row")
+let container = document.querySelector(".container")
+let containerPadding = 40
+function createList() {
+    let itemName = document.querySelector("input").value
+    console.log(itemName);
+    if (itemName === "") {
+        alert("You Need To Input Something First!")
+    } else{
+        
+    }
+}
+
+function checkOnClick() {
+
+}
