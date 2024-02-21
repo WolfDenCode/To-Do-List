5b5b7e473d8ae2bb776c79abd23f4f3261de42d6
WolfDenCode <wolfdencode@gmail.com>
2024-02-21 16:03:44 +0100

    Without Animation Adding

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-let parentElement = document.querySelector(".row")
-let container = document.querySelector(".container")
-let containerPadding = 40
-function createList() {
-    let itemName = document.querySelector("input").value
-    console.log(itemName);
-    if (itemName === "") {
-        alert("You Need To Input Something First!")
-    } else{
-        
-    }
+const elementList = document.querySelector(".element-list");
+const input = document.querySelector("input");
+const submit = document.querySelector("#submit");
+function addItem() {
+  let itemName = input.value;
+
+  const newItemDiv = document.createElement("div");
+  const itemCheckBtn = document.createElement("button");
+  const itemText = document.createElement("li");
+  const itemDeleteBtn = document.createElement("button");
+
+  newItemDiv.appendChild(itemCheckBtn);
+  newItemDiv.appendChild(itemText);
+  newItemDiv.appendChild(itemDeleteBtn);
+
+  itemCheckBtn.classList.add("checker");
+  itemCheckBtn.addEventListener("click", () => {
+    checkOnClick(itemCheckBtn);
+  });
+  itemText.innerHTML = itemName;
+  itemDeleteBtn.classList.add("delete");
+  itemDeleteBtn.innerHTML = "X";
+  itemDeleteBtn.addEventListener("click", () => {
+    elementList.removeChild(newItemDiv);
+  });
+
+  elementList.appendChild(newItemDiv);
 }
 
-function checkOnClick() {
+function checkOnClick(element) {
+  if (element.classList.contains("active")) {
+    element.classList.remove("active");
+  } else {
+    element.classList.add("active");
+  }
+}
 
+function toggleSubmit() {
+  if (input.value == "") {
+    submit.disabled = true;
+  } else {
+    submit.disabled = false;
+  }
 }
